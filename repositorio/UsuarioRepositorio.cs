d18d50aff3975f3dc7ed62a6c1124e523801f3a2
Julia Sant'Anna <juliasantanna.souza@gmail.com>
2023-05-18 10:52:43 -0300

    add UsuarioRepositorio e IUsuarioRepositorio
    Co-authored-by: GuilhermeSoaress <guilhermesoares278@gmail.com>

---
+++ repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using dominio;
+using dominio.Enums;
+using repositorio.Contexto;
+using repositorio.Interfaces;
+using System.Collections.Generic;
+using static repositorio.Contexto.ResolverContexto;
+
+namespace repositorio
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly IContexto contexto;
+
+        public UsuarioRepositorio(ResolverContextoDelegate resolverContexto)
+        {
+            contexto = resolverContexto(ContextoBancoDeDados.Postgresql);
+        }
+
+       
+
+        public UsuarioDnit Obter(string email, string senha)
+        {
+            var sql = @"SELECT * FROM public.usuario WHERE email = @Email AND senha = @Senha ";
+
+
+            var parametro = new
+            {
+                Email = email,
+                Senha = senha
+            };
+
+            var usuarioDnit = contexto?.Conexao.QuerySingle<UsuarioDnit>(sql, parametro);
+
+            return usuarioDnit;
+        }
+
+    }
+}
