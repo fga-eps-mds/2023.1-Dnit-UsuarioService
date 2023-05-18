ed5910c6610e4368a022ce1591d2752a00127d8b
avmricardo <ricardomaciel.unb@gmail.com>
2023-05-18 00:07:19 -0300

    Adicionando arquivos de UsuarioDNT no dominio e repositorio
    Co-authored-by: Carolina Barbosa <carolinabarbosab09@gmail.com>

---
+++ service/UsuarioService.cs
@@ -0,0 +1,20 @@
+﻿using dominio;
+using repositorio.Interfaces;
+using service.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace service
+{
+    class UsuarioService : IUsuarioService
+    {
+        /// <summary>
+        /// Função para pegar UsuarioDNIT retirado do repositorio
+        /// retorno null até a implementação correta
+        /// </summary>
+        public UsuarioDNIT Obter(int id)
+        {
+           return null;
+        }
+    }
+}
