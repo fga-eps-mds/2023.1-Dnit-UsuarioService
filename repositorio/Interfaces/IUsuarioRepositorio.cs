30675b1c68c8fdd6fb610819bf80c9e8106ba1d2
guilh <guilhermesoares278@gmail.com>
2023-05-18 11:22:26 -0300

    att UsuarioService e IUsuarioRepositorio
    Co-authored-by:JuliaSSouza <juliasantanna.souza@gmail.com>

--- repositorio/Interfaces/IUsuarioRepositorio.cs
+++ repositorio/Interfaces/IUsuarioRepositorio.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using dominio;
 
 namespace repositorio.Interfaces
 {
-    internal class UsuarioRepositorio
+    public interface IUsuarioRepositorio
     {
+        public UsuarioDnit Obter(string email, string senha);
     }
 }
--- service/UsuarioService.cs
+++ service/UsuarioService.cs
@@ -1,7 +1,36 @@
 using dominio;
+using repositorio.Interfaces;
+using repositorio.Contexto;
 using service.Interfaces;
+
 
 public class usuarioService : IUsuarioService
 {
-    
+    private readonly IUsuarioRepositorio usuarioRepositorio;
+
+    public usuarioService(IUsuarioRepositorio usuarioRepositorio)
+    {
+        this.usuarioRepositorio = usuarioRepositorio;
+    }
+
+    public UsuarioDnit Obter(string email, string senha)
+    {
+        UsuarioDnit usuarioDnit = usuarioRepositorio.Obter(email, senha);
+        
+        return usuarioDnit;
+    }
+
+    public void Login(string email, string senha) // tipo
+    {
+        email = emailtxt.Text; // emailtxt seria o campo de email do front-end
+        senha = senhatxt.Text;
+
+        UsuarioDnit usuarioDnit = usuarioRepositorio.Obter(email, senha);
+    }
+
+    public bool validaLogin() // parâmetros
+    {
+        //valida se a senha digitada confere com a do banco
+
+    }
 }
