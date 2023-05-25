fdb43150f5447468e460941f3891b8b7e7a772d2
avmricardo <ricardomaciel.unb@gmail.com>
2023-05-24 23:24:35 -0300

    #10 classe usuario terceiro e função cadastrar terceiro em repositorio

--- dominio/UsuarioDnit.cs
+++ dominio/UsuarioDnit.cs
@@ -1,11 +1,7 @@
 namespace dominio
 {
-    public class UsuarioDnit
+    public class UsuarioDnit : Usuario
     {
-        public int Id { get; set; }
-        public string Email { get; set; }
-        public string Senha { get; set; }
-        public string Nome { get; set; }
         public int UF { get; set; }
 
         public UsuarioDnit(string email, string senha, string nome, int uf)
@@ -29,6 +25,5 @@ namespace dominio
             this.Nome = nome;
             this.UF = id;
         }
-
     }
 }
---
+++ dominio/UsuarioTerceiro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class UsuarioTerceiro : Usuario
+    {
+        public string CNPJempresa { get; set; }
+
+        public UsuarioTerceiro(string nome, string email, string senha, string CNPJempresa)
+        {
+            this.Nome = nome;
+            this.Email = email;
+            this.Senha = senha;
+            this.CNPJempresa = CNPJempresa;
+        }
+    }
+}
