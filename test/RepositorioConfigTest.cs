ca6161e1b6fa632e73d4b35edb9ffe644a15f57b
lucas229 <lucasro229@gmail.com>
2023-06-29 18:59:31 -0300

    #33 Adição de testes para configuração de DI

---
+++ test/RepositorioConfigTest.cs
@@ -0,0 +1,32 @@
+﻿using app.DI;
+using Microsoft.Extensions.DependencyInjection;
+using repositorio.Interfaces;
+using repositorio;
+using Xunit;
+
+namespace test
+{
+    public class RepositorioConfigTest
+    {
+        public class RepositoriosConfigTests
+        {
+            [Fact]
+            public void AddConfigRepositorios_QuandoMetodoForChamado_DeveConfigurarInjecaoDeDependencia()
+            {
+                var services = new ServiceCollection();
+
+                RepositoriosConfig.AddConfigRepositorios(services);
+
+                Assert.Contains(services, descriptor =>
+                    descriptor.ServiceType == typeof(IUnidadeFederativaRepositorio) &&
+                    descriptor.ImplementationType == typeof(UnidadeFederativaRepositorio) &&
+                    descriptor.Lifetime == ServiceLifetime.Scoped);
+
+                Assert.Contains(services, descriptor =>
+                    descriptor.ServiceType == typeof(IUsuarioRepositorio) &&
+                    descriptor.ImplementationType == typeof(UsuarioRepositorio) &&
+                    descriptor.Lifetime == ServiceLifetime.Scoped);
+            }
+        }
+    }
+}
