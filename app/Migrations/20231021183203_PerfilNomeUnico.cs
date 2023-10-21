83856c17b8052498960664bd38ca7c92021a1569
Daniel Porto <danielportods@gmail.com>
2023-10-21 16:16:11 -0300

    feat: define o nome do perfil como campo unico

---
+++ app/Migrations/20231021183203_PerfilNomeUnico.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace app.Migrations
+{
+    /// <inheritdoc />
+    public partial class PerfilNomeUnico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Perfis_Nome",
+                table: "Perfis",
+                column: "Nome",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Perfis_Nome",
+                table: "Perfis");
+        }
+    }
+}
