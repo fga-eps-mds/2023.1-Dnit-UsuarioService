e73e620dfac8dea5bf4000bbab67800ce3715f85
Daniel Porto <danielportods@gmail.com>
2023-10-25 05:43:11 -0300

    fix: corrige assinatura da funcao na interface do perfilService

--- app/Services/Interfaces/IPerfilService.cs
+++ app/Services/Interfaces/IPerfilService.cs
@@ -8,7 +8,7 @@ namespace app.Services.Interfaces
         Perfil CriarPerfil(Perfil perfil, List<Permissao> permissoes);
         Task<Perfil> EditarPerfil(Perfil perfil, List<Permissao> permissoes);
         Task ExcluirPerfil(Guid id);
-        Task<List<Perfil>> ListarPerfisAsync(int pageIndex, int pageSize, string? nome);
+        Task<List<Perfil>> ListarPerfisAsync(int pageIndex, int pageSize, string? nome = null);
         Task<Perfil?> ObterPorIdAsync(Guid id);
     }
 }
