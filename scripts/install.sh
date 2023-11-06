117b683d8d0eab5df4bbb4393cd805cd5755ea97
Thiago Paiva <thiagohdaqw@gmail.com>
2023-11-05 22:52:43 -0300

    deploy: adicionando caminho para as variaveis do servico
    Co-authored-by: Eduardo Sandes <eduardo.sandes6@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,6 +14,7 @@ Description = Deploy Dnit Service
 
 [Service]
 WorkingDirectory=$WORKDIR
+EnvironmentFile=/etc/dnit-deploy/env
 ExecStart=$WORKDIR/.venv/bin/python -m uvicorn main:app --host 0.0.0.0 --port 30001
 Restart=on-failure
 
