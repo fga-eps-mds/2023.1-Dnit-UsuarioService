1971e549d07efbae2e8a4e24259a760ed4b4a74c
Thiago Paiva <thiagohdaqw@gmail.com>
2023-11-08 22:10:52 -0300

    devops: criando pacotes de deploy continuo
    Será usado nos outros servicos
    
    Co-authored-by: Wagner M Cunha <wagnermc506@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ WORKDIR=$(pwd)
 python3 -m venv .venv
 source $WORKDIR/.venv/bin/activate
 
-pip install -r "${WORKDIR}/requirements.txt"
+pip install .
 
 cat > /etc/systemd/system/dnit-deploy.service <<EOF
 [Unit]
@@ -15,7 +15,7 @@ Description = Deploy Dnit Service
 [Service]
 WorkingDirectory=$WORKDIR
 EnvironmentFile=/etc/dnit-deploy/env
-ExecStart=$WORKDIR/.venv/bin/python -m uvicorn main:app --host 0.0.0.0 --port 30001
+ExecStart=$WORKDIR/.venv/bin/python -m uvicorn dnit_updater:app --host 0.0.0.0 --port 30001
 Restart=on-failure
 
 [Install]
